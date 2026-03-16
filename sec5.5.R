c517fd7eecfe9b6b93ac9ce26192d9667d1d120f
MrlsHfr <marlies.hafer@tu-dortmund.de>
2026-03-16 08:38:26 +0100

    Update sec5.5.R

--- sec5.5.R
+++ sec5.5.R
@@ -65,17 +65,14 @@ for(i in 1:1000)
   (beta_first<-lm(Y_S~X_S+Z_S)$coefficients)
   Y_second<-cbind(rep(1,n),X,Z)%*%beta_first
   (beta_second<-lm(Y_second~X)$coefficients)  
-  g<-function(x){beta_second[1]+beta_second[2]*x}
-  rr[i]<-g(1)-g(0)
+  rr[i]<-beta_second[2]
   
   #TSR
-  f<-function(x){beta_first[1]+beta_first[2]*x+sum(beta_first[-c(1,2)]*apply(Z,2,mean))}
-  tsr[i]<-f(1)-f(0)
+  tsr[i]<-beta_first[2]
   
   #naive
   (beta_naive<-lm(Y_S~X_S)$coefficients)
-  h<-function(x){beta_naive[1]+beta_naive[2]*x}
-  na[i]<-h(1)-h(0)
+  na[i]<-beta_naive[2]
 }
 
 par(mfrow=c(1,2),cex.axis = 1.6,cex.lab=1.6,oma=c(0,0.5,0,0),mar=c(7,7,3,3))
@@ -99,14 +96,14 @@ for(i in 1:1000)
 
   #randomized treatment
   X<-runif(n,-1,1)
-  discrete <- sample(values, size = 28, replace = TRUE, prob = probabilities)
+  discrete <- sample(values, size = 27, replace = TRUE, prob = probabilities)
   
   #add confounding
   C<-rnorm(n,2,2)
   X<-X+C
   
   #generate target Y
-  Y<-cbind(rep(1,n),X,Z,C)%*%discrete+rnorm(n)
+  Y<-cbind(rep(1,n),X,Z)%*%discrete+C+rnorm(n)
 
   #induce selection bias based on X and one variable in Z
   S<-ifelse((X+Z[,3]>0.7),1,0)
@@ -122,21 +119,18 @@ for(i in 1:1000)
   (beta_first<-lm(Y_S~X_S+Z_S+C_S)$coefficients)
   Y_second<-cbind(rep(1,n),X,Z,C)%*%beta_first
   (beta_second<-lm(Y_second~X)$coefficients)  
-  g<-function(x){beta_second[1]+beta_second[2]*x}
-  rr[i]<-g(1)-g(0)
+  rr[i]<-beta_second[2]
   
   #TSR
-  f<-function(x){beta_first[1]+beta_first[2]*x+sum(beta_first[-c(1,2)]*apply(cbind(Z,C),2,mean))}
-  tsr[i]<-f(1)-f(0)
+  tsr[i]<-beta_first[2]
   
   #naive
   (beta_naive<-lm(Y_S~X_S)$coefficients)
-  h<-function(x){beta_naive[1]+beta_naive[2]*x}
-  na[i]<-h(1)-h(0)
+  na[i]<-beta_naive[2]
 }
 
 boxplot(cbind(tsr-t,rr-t,na-t),names=c("TSR","RR","naive"),col=c("lightgreen","lightblue",adjustcolor("orange", alpha = 0.35)),ylab="Bias",xlab="",main="Selection Bias and Confounding")
 abline(h=0,col="gray")
 apply(cbind(tsr-t,rr-t,na-t),2,mean)
-#[1] 0.004511279 0.918960668 1.037122255
+#[1] -0.003403171  0.922645222  1.050728208
 
