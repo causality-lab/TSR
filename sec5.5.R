3a0167f73f32984ab1ded4e3f90243eda8f3bc6f
MrlsHfr <marlies.hafer@tu-dortmund.de>
2026-03-19 12:08:53 +0100

    Update sec5.5.R

--- sec5.5.R
+++ sec5.5.R
@@ -16,6 +16,9 @@
 # We use the original dataset without deleted observation, which can be found here:
 # https://www.tandfonline.com/doi/suppl/10.1198/jcgs.2010.08162?scroll=top
 
+#use package for cross-validation
+library(glmnet)
+
 load(file="sim.data")
 length(colnames(imp1))
 #[1] 29
@@ -34,10 +37,12 @@ Z<-cbind(scale(imp1[,2]),scale(imp1[,3]),scale(imp1[,4]),scale(imp1[,5]), scale(
 
 set.seed(1)
 
-tsr <- NULL
-rr  <- NULL
-na  <- NULL
-t   <- NULL
+tsr   <- NULL
+tsr_r <- NULL
+rr    <- NULL
+rr_r  <- NULL
+na    <- NULL
+t     <- NULL
 
 for(i in 1:1000)
 {
@@ -67,26 +72,42 @@ for(i in 1:1000)
   (beta_second<-lm(Y_second~X)$coefficients)  
   rr[i]<-beta_second[2]
   
+  #RR ridge 
+  lambda_seq <- 10^seq(2, -2, by = -.1)
+  ridge_cv <- cv.glmnet(cbind(X_S,Z_S), Y_S, alpha = 0, lambda = lambda_seq)
+  best_lambda <- ridge_cv$lambda.min
+  best_ridge <- glmnet(cbind(X_S,Z_S), Y_S, alpha = 0, lambda = best_lambda)
+  beta_first_ridge<-as.vector(coef(best_ridge))
+  Y_second_ridge<-cbind(rep(1,n),X,Z)%*%beta_first_ridge
+  (beta_second_ridge<-lm(Y_second_ridge~X)$coefficients)  
+  rr_r[i]<-beta_second_ridge[2]
+  
   #TSR
   tsr[i]<-beta_first[2]
+  
+  #TSR ridge
+  tsr_r[i]<-beta_first_ridge[2]
   
   #naive
   (beta_naive<-lm(Y_S~X_S)$coefficients)
   na[i]<-beta_naive[2]
 }
 
-par(mfrow=c(1,2),cex.axis = 1.6,cex.lab=1.6,oma=c(0,0.5,0,0),mar=c(7,7,3,3))
-boxplot(cbind(tsr-t,rr-t,na-t),names=c("TSR","RR","naive"),col=c("lightgreen","lightblue",adjustcolor("orange", alpha = 0.35)),ylab="Bias",xlab="", main="Selection Bias")
+par(mfrow=c(1,1),cex.axis = 2,cex.lab=2,cex.main=2.3,oma=c(0,0.5,0,0),mar=c(7,7,7,3))
+boxplot(cbind(tsr-t,tsr_r-t,rr-t,rr_r-t,na-t),names=c("","","","",""),col=c("lightgreen","lightgreen","lightblue","lightblue",adjustcolor("orange", alpha = 0.35)),ylab="Bias",xlab="",main="Selection Bias")
+axis(1,at = 1:5,labels = c("TSR\n","TSR \n(ridge)","RR\n", "RR \n(ridge)","naive\n"),tick = FALSE,line = 2.2)   
 abline(h=0,col="gray")
-apply(cbind(tsr-t,rr-t,na-t),2,mean)
-#[1]  0.006402254 -0.007051418  0.653637049
+apply(cbind(tsr-t,tsr_r-t,rr-t,rr_r-t,na-t),2,mean)
+#[1] -0.0006897514 -0.0110324166 -0.0066913512 -0.0168984409  0.6385973328
 
 #adding confounding
 
-tsr<-NULL
-rr<-NULL
-na<-NULL
-t<-NULL
+tsr   <- NULL
+tsr_r <- NULL
+rr    <- NULL
+rr_r  <- NULL
+na    <- NULL
+t     <- NULL
 
 for(i in 1:1000)
 {
@@ -121,16 +142,31 @@ for(i in 1:1000)
   (beta_second<-lm(Y_second~X)$coefficients)  
   rr[i]<-beta_second[2]
   
+  #RR ridge 
+  lambda_seq <- 10^seq(2, -2, by = -.1)
+  ridge_cv <- cv.glmnet(cbind(X_S,Z_S,C_S), Y_S, alpha = 0, lambda = lambda_seq)
+  best_lambda <- ridge_cv$lambda.min
+  best_ridge <- glmnet(cbind(X_S,Z_S,C_S), Y_S, alpha = 0, lambda = best_lambda)
+  beta_first_ridge<-as.vector(coef(best_ridge))
+  Y_second_ridge<-cbind(rep(1,n),X,Z,C)%*%beta_first_ridge
+  (beta_second_ridge<-lm(Y_second_ridge~X)$coefficients)  
+  rr_r[i]<-beta_second_ridge[2]  
+    
   #TSR
   tsr[i]<-beta_first[2]
+  
+  #TSR_ridge
+  tsr_r[i]<-beta_first_ridge[2]
   
   #naive
   (beta_naive<-lm(Y_S~X_S)$coefficients)
   na[i]<-beta_naive[2]
 }
 
-boxplot(cbind(tsr-t,rr-t,na-t),names=c("TSR","RR","naive"),col=c("lightgreen","lightblue",adjustcolor("orange", alpha = 0.35)),ylab="Bias",xlab="",main="Selection Bias and Confounding")
+par(mfrow=c(1,1),cex.axis = 2,cex.lab=2,cex.main=2.3,oma=c(0,0.5,0,0),mar=c(7,7,7,3))
+boxplot(cbind(tsr-t,tsr_r-t,rr-t,rr_r-t,na-t),names=c("","","","",""),col=c("lightgreen","lightgreen","lightblue","lightblue",adjustcolor("orange", alpha = 0.35)),ylab="Bias",xlab="",main="Selection Bias\n and Confounding")
+axis(1,at = 1:5,labels = c("TSR\n","TSR \n(ridge)","RR\n", "RR \n(ridge)","naive\n"),tick = FALSE,line = 2.2)   
 abline(h=0,col="gray")
-apply(cbind(tsr-t,rr-t,na-t),2,mean)
+apply(cbind(tsr-t,tsr_r-t,rr-t,rr_r-t,na-t),2,mean)
 #[1] -0.003403171  0.922645222  1.050728208
-
+#[1] -0.0005705826  0.0085567640  0.9213210546  0.9201403989  1.0540470984
